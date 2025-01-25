08447e9cb10bbe4f52c7a23f50c0b90ecf4c92ff
Ardeleanu Vlad-George <ardeleanu_vlad@ymail.com>
2025-01-25 20:11:50 +0200

    It doesn't work as expected

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main(){
-  std::cout<<"Hello, color shifts!\n";
+  sf::Image img;
+  int temp;
+  img.loadFromFile("data/green_gecko.jpg");
+  sf::Uint8 *data = (sf::Uint8*) img.getPixelsPtr();
+  sf::Vector2u size = img.getSize();
+  int x = size.x, y = size.y;
+  for(int itr=0; itr < x*y; itr++){
+    temp = data[4*itr];
+    data[4*itr] = data[4*itr+1];
+    data[4*itr+1] = temp;
+    /*data[4*itr]+=(sf::Uint8)200;*/
+    /*data[4*itr+1]+=(sf::Uint8)200;*/
+    /*data[4*itr+2]+=(sf::Uint8)200;*/
+  }
+  img.saveToFile("data/shifted_gecko.jpg");
   return 0;
 }
