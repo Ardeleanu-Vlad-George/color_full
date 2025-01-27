6d4dbc377f56a6d78038fb7e4242cdcf6c348640
Ardeleanu Vlad-George <ardeleanu_vlad@ymail.com>
2025-01-27 07:05:32 +0200

    Added a nested class

--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,42 @@ class redgrnblu;
 
 std::ostream& operator<<(std::ostream&, redgrnblu&);
 
-class redgrnblu{ private:
+class redgrnblu{ 
+private:
   int r, g, b;
 public:
   redgrnblu(char *code){
-    sscanf(code, "%02x%02x%02x", &r, &g, &b);
+    sscanf(code, "#%02x%02x%02x", &r, &g, &b);
+  }
+  operator sf::Color(){
+    return sf::Color(r, g, b);
   }
   friend std::ostream& operator<<(std::ostream&, redgrnblu&);
+  class diff{
+  private:
+    int dr, dg, db;
+  public:
+    diff(redgrnblu& c1, redgrnblu& c2)
+      :dr(c1.r - c2.r),dg(c1.g - c2.g),db(c1.b - c2.b)
+        {}
+    std::string to_string(){
+      char rslt[211];
+      sprintf(rslt, "[%+04d]:[%+04d]:[%+04d]", dr, dg, db);
+      return std::string(rslt);
+    }
+  };
+
+  diff operator-(redgrnblu& c){
+    return diff(*this, c);
+  }
 };
 
 std::ostream& operator<<(std::ostream& os, redgrnblu& rgb){
   return os<<'('<<rgb.r<<", "<<rgb.g<<", "<<rgb.b<<')';
 }
 
-
 void log_help(std::ostream& os){
-  os<<"Give two RGB hex codes, '#' must be omited\n";
+  os<<"Give two RGB hex codes, '#' must be included\n";
 }
 
 int main(int argc, char *argv[]){
@@ -34,23 +54,9 @@ int main(int argc, char *argv[]){
     return -1;
   }
   redgrnblu source(argv[1]), target(argv[2]);
-  std::cout<<"1st color:"<<source<<'\n';
-  std::cout<<"2nd color:"<<target<<'\n';
-  return 0;
-  sf::Image img;
-  int temp;
-  img.loadFromFile("data/green_gecko.jpg");
-  sf::Uint8 *data = (sf::Uint8*) img.getPixelsPtr();
-  sf::Vector2u size = img.getSize();
-  int x = size.x, y = size.y;
-  for(int itr=0; itr < x*y; itr++){
-    temp = data[4*itr];
-    data[4*itr] = data[4*itr+1];
-    data[4*itr+1] = temp;
-    /*data[4*itr]+=(sf::Uint8)200;*/
-    /*data[4*itr+1]+=(sf::Uint8)200;*/
-    /*data[4*itr+2]+=(sf::Uint8)200;*/
-  }
-  img.saveToFile("data/shifted_gecko.jpg");
+  std::cout<<"1st color :"<<source<<'\n';
+  std::cout<<"2nd color :"<<target<<'\n';
+  redgrnblu::diff shift = source-target;
+  std::cout<<"difference:"<<shift.to_string()<<'\n';
   return 0;
 }
